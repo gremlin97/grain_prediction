91567f511e7afbe8168385e90fc4d576fa7ebd14
Yiyu <yiyuli233@gmail.com>
2018-04-01 21:48:35 +0800

    trd ac

---
+++ DL_data/tr_lac/statistic_accuracy.py
@@ -0,0 +1,27 @@
+import os
+import numpy as np
+
+for dir_m in os.listdir('./'):
+    if os.path.isfile(dir_m):
+        continue
+    writer = open(dir_m + '.csv', 'w')
+    whole = []
+    for file_b in os.listdir(dir_m):
+        ac_list = []
+        f_path = os.path.join(dir_m, file_b)
+        with open(f_path) as f:
+            mac = 0
+            b_list = []
+            for line in f:
+                ac = float(line.strip().split(',')[1])
+                ac_list.append(ac)
+
+        mean_ac = np.mean(ac_list)
+        whole.append(mean_ac)
+
+        writer.write(file_b[:-3] + ',' + str(mean_ac) + '\n')
+    writer.write('var,'+str(np.nanvar(whole)) + '\n')
+    writer.write('mean,'+str(np.nanmean(whole)))
+
+    writer.close()
+
